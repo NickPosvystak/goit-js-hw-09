612a99895fed08ef4cfbc9989a44a25a5bc4a033
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-08-04 22:13:44 +0200

    update 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener('click', onClick);
+stopBtn.addEventListener('click', offClick);
+
+function onClick() {
+  if (!timerId) {
+    timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }
+}
+
+function offClick() {
+  if (timerId) {
+    clearInterval(timerId);
+    timerId = null;
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
