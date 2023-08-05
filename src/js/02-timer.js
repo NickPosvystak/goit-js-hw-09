97d06a38b5fce60db7654c9d8818fd5c6251176b
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-08-05 19:41:18 +0200

    update 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,96 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+import swal from 'sweetalert';
+
+// swal("Hello world!");
+
+const inputEl = document.getElementById('datetime-picker');
+const btn = document.querySelector('[data-start]');
+const secondsEl = document.querySelector('[data-seconds]');
+const minutesEl = document.querySelector('[data-minutes]');
+const hoursEl = document.querySelector('[data-hours]');
+const daysEl = document.querySelector('[data-days]');
+
+let counter;
+btn.disabled = true;
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (selectedDate < currentDate) {
+        swal('Please choose a date in the future');
+      btn.disabled = true;
+    } else {
+      btn.disabled = false;
+    }
+  },
+};
+const displayTime = timeBack => {
+  const { days, hours, minutes, seconds } = convertMs(timeBack);
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+  if (timeBack <= 0) {
+    clearInterval(counter);
+    daysEl.textContent = '00';
+    hoursEl.textContent = '00';
+    minutesEl.textContent = '00';
+    secondsEl.textContent = '00';
+  }
+};
+
+const flatFunc = flatpickr(inputEl, options);
+
+btn.addEventListener('click', onClick);
+
+function onClick() {
+  const selectedDate = flatFunc.selectedDates[0];
+  const currentDate = new Date();
+
+  if (selectedDate > currentDate) {
+    const time = selectedDate.getTime() - currentDate.getTime();
+
+    displayTime(time);
+
+    counter = setInterval(() => {
+      const timeUpdate = selectedDate.getTime() - new Date().getTime();
+      displayTime(timeUpdate);
+
+      if (timeUpdate <= 0) {
+        clearInterval(counter);
+      }
+    }, 1000);
+  }
+}
